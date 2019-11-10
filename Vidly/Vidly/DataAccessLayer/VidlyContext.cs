d46e0c5bfee460fa985092197dd1a7c72d1ec4dd
Mahlatse Tshehla <mahlatset@capfin.co.za>
2019-11-10 09:26:00 +0200

    added an entity code first framework

---
+++ Vidly/Vidly/DataAccessLayer/VidlyContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.DataAccessLayer
+{
+    public class VidlyContext : DbContext 
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+
+        public VidlyContext()
+            : base("name=DeafaultConnection")
+        {
+
+        }
+    }
+}
